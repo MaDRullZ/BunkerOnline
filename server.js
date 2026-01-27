37271eb88e393c1b334780ad1c801671160265ec
MaDRullZ <timurmadrullz@gmail.com>
2026-01-27 17:01:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,26 @@
-// server.js
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 
 const app = express();
+app.use(express.static("public"));
+
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "*" }, // разрешаем подключения с любого URL
+  cors: { origin: "*" }
 });
 
-app.use(express.static(".")); // отдаём все файлы проекта (index.html, style.css)
-
 io.on("connection", (socket) => {
-  console.log("Новое подключение:", socket.id);
-
-  // Создание комнаты
   socket.on("createRoom", (callback) => {
-    const roomCode = Math.random().toString(36).substring(2, 7).toUpperCase();
+    const roomCode = Math.random().toString(36).substr(2, 5).toUpperCase();
     socket.join(roomCode);
-    console.log(`Комната создана: ${roomCode}`);
     callback(roomCode);
   });
 
-  // Присоединение к комнате
   socket.on("joinRoom", (roomCode, callback) => {
-    const rooms = io.sockets.adapter.rooms;
-    if (rooms.has(roomCode)) {
-      socket.join(roomCode);
-      callback(true);
-      console.log(`Игрок ${socket.id} присоединился к комнате ${roomCode}`);
-    } else {
-      callback(false);
-    }
+    const room = io.sockets.adapter.rooms.get(roomCode);
+    callback(room ? true : false);
   });
 });
 
-// Сервер запускается на Render или локально
-server.listen(process.env.PORT || 3000, () => {
-  console.log("Сервер запущен!");
-});
+server.listen(process.env.PORT || 3000);
